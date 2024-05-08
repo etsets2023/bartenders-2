899bc2bacf9a47456257bb37bdce7c1eec57d2b7
ElaineTavares <etsets.tavares@gmail.com>
2024-05-08 11:14:46 -0300

    navbar

--- js/script.js
+++ js/script.js
@@ -31,6 +31,51 @@ const closeModalButton = document.querySelector('.btn_fechar_bartender')
 const modal = document.getElementById('modal')
 const fade = document.getElementById('fade')
 
+class MobileNavbar{
+    constructor(mobileMenu, navList, navLinks){
+        this.mobileMenu = document.querySelector(mobileMenu)
+        this.navList = document.querySelector(navList)
+        this.navLinks = document.querySelectorAll(navLinks)
+        this. activeClass = 'active'
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+
+    animateLinks(){
+        this.navLinks.forEach((link, index)=>{
+            link.style.animation
+            ? (link.style.animation = "")
+            : (link.style.animation = `navLinkFade 0.5s ease forwards 0.3s`)
+        })
+    }
+
+
+    handleClick(){
+        this.navList.classList.toggle(this.activeClass);
+        this.animateLinks();
+    }
+
+    addClickEvent(){
+        this.mobileMenu.addEventListener('click', this.handleClick);
+    }
+
+    init(){
+        if (this.mobileMenu){
+            this.addClickEvent();
+        } 
+        return this;
+    }
+}
+
+
+
+const mobileNavbar = new MobileNavbar(
+    ".bi-list",
+    ".links_nav",
+    ".links_nav li"
+);
+mobileNavbar.init();
 
 
 whisky.addEventListener('mouseover', ()=>{
@@ -183,6 +228,8 @@ const toggleModalContrate = () => {
    modalContrate.classList.toggle('hide')
    fadeContrate.classList.toggle('hide')
 }
+
+
 
 
 
